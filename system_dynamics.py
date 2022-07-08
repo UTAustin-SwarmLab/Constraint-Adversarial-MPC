b5cbb889dfb864e7c376a8267c05801392b23990
d31003 <d31003@gmail.com>
2022-07-08 23:29:49 +0800

    change to multiple time series and 1 controller

--- system_dynamics.py
+++ system_dynamics.py
@@ -12,7 +12,7 @@ m = parsed_yaml_file['m']
 p = parsed_yaml_file['p']
 sample_n = parsed_yaml_file['sample_n']
 
-def CtrlTask(random_seed=326, x0=1, T=T):
+def CtrlTask(s, random_seed=326, x0=1, T=T):
     #This function returns the co-design matrix, Phi.
     #S is not related to it so we assume it is 0 in this function.
 
@@ -55,7 +55,8 @@ def CtrlTask(random_seed=326, x0=1, T=T):
 
     s = np.array(s)
 
-    K = np.zeros((m*T, m*T)) + R
+    # K = np.zeros((m*T, m*T)) + R
+    K = np.kron(np.eye(T,dtype=int), R)
     k = np.zeros((m*T,1))
     L = np.zeros((m*T, p*T))
 
@@ -82,20 +83,8 @@ def CtrlTask(random_seed=326, x0=1, T=T):
     Phi = np.dot(L.T, np.linalg.inv(K))
     Phi = np.dot(Phi, L)
 
-    # print("Q", np.allclose(np.linalg.cholesky(Q) @ np.linalg.cholesky(Q).T, Q))
-    # print("R", np.allclose(np.linalg.cholesky(R) @ np.linalg.cholesky(R).T, R))
-    # print("K", np.allclose(np.linalg.cholesky(K) @ np.linalg.cholesky(K).T, K))
-    # print("K sym", np.allclose(K.T, K))
-    # print("K-1", np.allclose(np.linalg.cholesky(np.linalg.inv(K)) @ np.linalg.cholesky(np.linalg.inv(K)).T, np.linalg.inv(K)))
-    # val, vec = np.linalg.eigh(np.linalg.inv(K))
-    # print("K-1 val:", val[0:10], min(val), max(val))
-    # print(np.allclose(np.linalg.inv(K)@K, np.eye(K.shape[0], K.shape[1])))
-    # print(np.linalg.inv(K)@K)
-    # print("Phi", np.allclose(np.linalg.cholesky(Phi) @ np.linalg.cholesky(Phi).T, Phi))
-
     # xt
     x_list = [np.array(x0)]
-    x_list_ = [x0]
 
     for t in range(0, T-1):
         xt = np.dot(np.linalg.matrix_power(A, t+1), x0)
@@ -103,15 +92,105 @@ def CtrlTask(random_seed=326, x0=1, T=T):
         xt += np.dot(N_list[t], s)
         x_list.append(xt)
 
-        x = np.dot(A, x_list_[-1]) + np.dot(B, u_opt[t]) + np.dot(C, s[t])
-        x_list_.append(x)
-
     cost = u_opt.T @ K @ u_opt + 2 * k.T @ u_opt
-    for t in range(0, T-1):
+    for t in range(0, T):
         const = np.dot(np.linalg.matrix_power(A, t+1), x0) + N_list[t] @ s
         cost += const.T @ Q @ const
 
     return Phi, float(cost)
+
+
+def CtrlTaskIndentity(s, x0=1, T=T):
+    #This function returns the co-design matrix, Phi.
+    #S is not related to it so we assume it is 0 in this function.
+
+    #T: total time steps
+    #n: x-dim
+    #m: u-dim
+    #p: s-dim
+
+    # s = np.zeros((p*T,1))
+    s = s.T
+
+    A = np.eye(n, dtype=np.float64)
+    B = - np.eye(m, dtype=np.float64)
+    C = np.eye(p, dtype=np.float64)
+
+    M_list = []
+    N_list = []
+
+    for i in range(T):
+        if i == 0:
+            M = np.concatenate((B, np.zeros((n, m*(T-1)))), axis=1)
+            N = np.concatenate((C, np.zeros((n, p*(T-1)))), axis=1)
+            M_list.append(M)
+            N_list.append(N)
+            continue
+
+        _ = np.dot(A, M_list[-1][:, :m]).reshape((n, m))
+        __ = np.dot(A, N_list[-1][:, :p]).reshape((n, p))
+
+        M = np.concatenate((_, M_list[-1][:, :m*(T-1)]), axis=1)
+        N = np.concatenate((__, N_list[-1][:, :p*(T-1)]), axis=1)
+        M_list.append(M)
+        N_list.append(N)
+
+    # print(M_list[0:2], M_list[0].shape)
+
+    ### Q R must be PSD
+    Q = np.eye(m, dtype=np.float64)
+    R = np.eye(m, dtype=np.float64)
+
+    # K = np.zeros((m*T, m*T)) + R
+    K = np.kron(np.eye(T,dtype=int), R)
+    k = np.zeros((m*T, 1))
+    L = np.zeros((m*T, p*T))
+    # print(K, K.shape, T)
+
+    for t in range(T):
+        # K
+        _ = np.dot(M_list[t].T, Q)
+        _ = np.dot(_, M_list[t])
+        K += _
+
+        #k
+        __ = np.dot(M_list[t].T, Q)
+        # print((N_list[t].shape, s.shape))
+        Nts = np.dot(N_list[t], s)
+        A_pow_t1_x0 = np.dot(np.linalg.matrix_power(A, t+1), x0)
+        __ = np.dot(__, (A_pow_t1_x0 + Nts) )
+        k += __
+
+        #L
+        ___ = np.dot(M_list[t].T, Q)
+        ___ = np.dot(___, N_list[t])
+        L += ___
+
+    u_opt = -1 * np.dot(np.linalg.inv(K), k)
+
+    Phi = np.dot(L.T, np.linalg.inv(K))
+    Phi = np.dot(Phi, L)
+    
+    # xt
+    x_list = [np.array(x0)]
+    # x_list_ = [x0]
+
+    for t in range(0, T):
+        xtplus1 = np.dot(np.linalg.matrix_power(A, t+1), x0)
+        xtplus1 += np.dot(M_list[t], u_opt)
+        xtplus1 += np.dot(N_list[t], s)
+        x_list.append(xtplus1)
+
+        # x = np.dot(A, x_list_[-1]) + np.dot(B, u_opt[t]) + np.dot(C, s[t])
+        # x_list_.append(x)
+
+    cost = u_opt.T @ K @ u_opt + 2 * k.T @ u_opt
+    for t in range(0, T):
+        const = np.dot(np.linalg.matrix_power(A, t+1), x0) + N_list[t] @ s
+        cost += const.T @ Q @ const
+
+    return Phi, float(cost)
+
 
 def ConstaintedCtrlTaskParas(random_seed = 326, T=T):
     #This function returns the parameters (A, B, C, Q, R).
@@ -150,7 +229,7 @@ def ARIMA_gen(random_seed = 326, sample_n = sample_n):
     BETA = np.random.rand(sample_n,1)
     SIGMA = 0.1 * np.random.rand(sample_n,1)
 
-    S = np.zeros((MU.shape[0], p*T)) ### S0=1
+    S = np.zeros((MU.shape[0], p*T))
 
     for i in range(MU.shape[0]):
         mu = MU[i]
@@ -158,9 +237,9 @@ def ARIMA_gen(random_seed = 326, sample_n = sample_n):
         beta = BETA[i]
         sigma = SIGMA[i]
         w = np.random.randn(T) * sigma
-        s = np.zeros(T)
+        s = np.zeros(T) + 1 ### S0=1
 
-        for t in range(len(w)):
+        for t in range(1, len(w)):
             s[t] = mu + alpha * s[t-1] + w[t] + beta * w[t-1]
         
         S[i, :]=s
@@ -168,7 +247,8 @@ def ARIMA_gen(random_seed = 326, sample_n = sample_n):
     return S, MU, ALPLA, BETA, SIGMA
 
 if __name__ == '__main__':
-    Phi = CtrlTask()
-    S = ARIMA_gen
-    print(Phi.shape, S.shape)
+    # Phi = CtrlTask()
+    Phi, cost = CtrlTaskIndentity(s=np.zeros((1,p*T)))
+    S = ARIMA_gen()
+    # print(Phi.shape, S.shape)
 
